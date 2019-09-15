5c7299be5255c1cdfbc8c1ca786bd5f76e7a877a
seonghyeon <tjdgusdkf@gmail.com>
2019-09-15 18:50:27 +0900

    festival set

--- festival/urls.py
+++ festival/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+	path('foodtruck', views.foodtruck, name='fest_foodtruck'),
+	path('booth', views.booth, name='booth'),
+	path('stamp', views.stamp, name='stamp'),
+	path('contest/talent', views.talent, name='talent'),
+	path('contest/cheer', views.cheer, name='cheer'),
 ]
