a2f8f44b97ca495910be1e9f19375fcd094cb9c1
sneakstarberry <gihong2012@likelion.org>
2019-09-15 19:01:04 +0900

    make all urls

--- competition/urls.py
+++ competition/urls.py
@@ -3,13 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('event/', views.compete_event, name='event'),
     path('timeline/', views.compete_timeline , name='compete_timeline'),
     path('safety/', views.safety, name='safety'),
     path('seatmap/', views.seat_map, name='seat_map'),
     path('foodtruck', views.food_truck, name='compete_food_truck')
-=======
-    # path('home/', views.home, name='home'),
->>>>>>> 5c7299be5255c1cdfbc8c1ca786bd5f76e7a877a
 ]
