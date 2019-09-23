ed76d90ae0f0a10bc7b5556c36c463177a7e591b
seonghyeon <tjdgusdkf@gmail.com>
2019-09-24 07:36:28 +0900

    배성현 스탬프 거의 완성 ㅎㅎ?

--- festival/urls.py
+++ festival/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 	path('foodtruck', views.foodtruck, name='fest_foodtruck'),
-	path('booth', views.booth, name='booth'),
+	path('festmap', views.festmap, name='festmap'),
 	path('stamp', views.stamp, name='stamp'),
 	path('contest/talent', views.talent, name='talent'),
 	path('contest/cheer', views.cheer, name='cheer'),
--- festival/views.py
+++ festival/views.py
@@ -14,23 +14,8 @@ def my_custom_sql(self):
 def foodtruck (req):
 	return render(req, 'festival/foodtruck.html')
 
-def booth (req):
-	with connection.cursor() as cursor:
-		cursor.execute("SELECT * FROM Account")
-		rows = cursor.fetchall()
-	
-	expanded_rows = []
-
-	for row in rows:
-		expanded_row = { }
-		for idx , desc in enumerate(cursor.description):
-			expanded_row[desc[0]] = row[idx]
-		expanded_rows.append(expanded_row)
-
-	return JsonResponse({
-		'status' : 1,
-		'data' : expanded_rows,
-	}, safe=False)
+def festmap (req):
+	return render(req, 'festival/festmap.html')
 
 def stamp (req):
 	return render(req, 'festival/stamp.html')
